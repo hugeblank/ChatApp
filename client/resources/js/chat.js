ea54999b84bb1980d8200baff5b96180f39a8e98
Sean Oberoi <xsean.o@gmail.com>
2018-09-07 17:18:37 -0700

    frontend: recieves chat data

--- client/resources/js/chat.js
+++ client/resources/js/chat.js
@@ -9,7 +9,22 @@ $(document).ready(
                    // user entered their name
                    let name = $('#usernameInput').val();
                    console.log(name);
+                   
+                   $('#loginArea').fadeOut();
+                   $('#chatArea').fadeIn();
                }
+            }
+        );
+
+        $('#inputMessage').keypress(
+            (e) =>
+            {
+                if (e.which == 13)
+                {
+                    // user entered a message
+                    let message = $('#inputMessage').val();
+                    console.log(message);
+                }
             }
         );
     }
